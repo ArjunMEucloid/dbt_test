5a1f6215b00acaf85c5224d182e04d1c7491bad4
Arjun M <arjun.eucloid@outlook.com>
2024-10-09 05:53:50 +0000

    t

--- models/test/silver_fact.sql
+++ models/test/silver_fact.sql
@@ -9,7 +9,7 @@
 {{ config(materialized='table') }}
 
 
-SELECT ROW_NUMBER() OVER (ORDER BY ft.payment_key, coustomer_key, ft.time_key, item_key, store_key) AS fact_id,pk.trans_type, pk.bank_name, tm.date, ft.*
+SELECT ROW_NUMBER() OVER (ORDER BY ft.payment_key, customer_key, ft.time_key, item_key, store_key) AS fact_id,pk.trans_type, pk.bank_name, tm.date, ft.*
 FROM {{ref('stg_fact')}} ft
 LEFT JOIN {{ref('stg_trans')}} pk
 ON ft.payment_key = pk.payment_key
--- models/test/stg_customer.sql
+++ models/test/stg_customer.sql
@@ -8,14 +8,6 @@
 
 {{ config(materialized='table') }}
 
-with source_data as (
-
-    select 1 as id
-    union all
-    select null as id
-
-)
-
 select *
 from
 {{source('chatbot_data','dim_customer') }}
