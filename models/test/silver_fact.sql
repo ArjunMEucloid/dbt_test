dd2c323c66ad7c51753d2c6c63ad21c5815c645f
Arjun M <arjun.eucloid@outlook.com>
2024-10-14 07:14:58 +0000

    m

--- models/test/silver_fact.sql
+++ models/test/silver_fact.sql
@@ -1,8 +1,8 @@
 {{ config(materialized='table') }}
 
 
-SELECT ROW_NUMBER() OVER (ORDER BY ft.payment_key, customer_key, ft.time_key, item_key, store_key) AS fact_id,pk.trans_type, pk.bank_name, tm.date, ft.*
-FROM {{ref('stg_fact')}} ft
+SELECT ROW_NUMBER() OVER (ORDER BY ft.payment_key, ft.customer_key, ft.time_key, ft.item_key, ft.store_key) AS fact_id,pk.trans_type, pk.bank_name, tm.date, ft.*
+FROM {{ref('stg_fact')}}
 LEFT JOIN {{ref('stg_trans')}} pk
 ON ft.payment_key = pk.payment_key
 LEFT JOIN {{ref('stg_time')}} tm
